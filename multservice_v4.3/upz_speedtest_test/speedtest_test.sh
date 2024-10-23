96c72b958cafd7066ef21418fcc6baa282f1d8bc
Erick L Semindu <ericksocial21@gmail.com>
2024-10-23 12:39:29 +0200

    Fixes     Speedtest service now contain both iperf3 and ookla speed test        Although iperf3 offers a lot of flexibility when it comes to measurements        Most of the time its public server are not available hence hindering the measuremtns        Unless we setup the public available server, ookla speetest as as a backup.
        iperf3 and ookla speedtest environmental variables
        Envirionmental variables have been introduced.
            ENV ookla_speedtest=false to enable ookla speed test
            ENV iperf3_speedtest=false to enable iperf3 speed test
            ENV iperf3_server=169.150.238.161 to set the iperf server
            All the variables can be modifed at the balena cloud

--- multservice_v4.3/upz_speedtest_test/speedtest_test.sh
+++ multservice_v4.3/upz_speedtest_test/speedtest_test.sh
@@ -1,3 +1,70 @@
 #!/bin/sh
-# Run ookla speed test 
-speedtest --format=json > "/reports/speedtest_result_$(date +%Y%m%d%H%M%S).json"
+
+if [ "$ookla_speedtest" = "true" ] && [ "$iperf3_speedtest" = "true" ]; then
+   # Run ookla speed test 
+    speedtest --accept-license --accept-gdpr --format=json > "/reports/speedtest_result_$(date +"%Y-%m-%d_%I-%M%p").json"
+    
+
+    # Run iperf3 test 
+    current_datetime=$(date +"%Y-%m-%d_%I-%M%p") # Custom format: Year-Month-Day Hour
+    IPERF3_OUTPUT="/reports/iperf3_result_$current_datetime.txt"
+
+    # Run iperf3 test TCP
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "Running iperf3 TCP test at $current_datetime" >> $IPERF3_OUTPUT
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "" >> $IPERF3_OUTPUT
+
+    IPERF3_RESULTS=$(iperf3 -c $iperf3_server -p 5201)
+    echo "$IPERF3_RESULTS" >> $IPERF3_OUTPUT
+
+    # Run iperf3 test UDP
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "Running iperf3 UDP test at $current_datetime" >> $IPERF3_OUTPUT
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "" >> $IPERF3_OUTPUT
+
+    IPERF3_RESULTS=$(iperf3 -c $iperf3_server -u -b 50M -p 5201)
+    echo "$IPERF3_RESULTS" >> $IPERF3_OUTPUT
+
+    echo "Tests completed. Results saved to $IPERF3_OUTPUT."
+
+
+elif [ "$ookla_speedtest" = "true" ]; then
+    # Run ookla speed test 
+    speedtest --accept-license --accept-gdpr --format=json > "/reports/speedtest_result_$(date +"%Y-%m-%d_%I-%M%p").json"
+ 
+elif [ "$iperf3_speedtest" = "true" ]; then
+ # Run ookla speed test 
+    speedtest --accept-license --accept-gdpr --format=json > "/reports/speedtest_result_$(date +"%Y-%m-%d_%I-%M%p").json"
+    
+
+    # Run iperf3 test 
+    current_datetime=$(date +"%Y-%m-%d_%I-%M%p") # Custom format: Year-Month-Day Hour
+    IPERF3_OUTPUT="/reports/iperf3_result_$current_datetime.txt"
+
+    # Run iperf3 test TCP
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "Running iperf3 TCP test at $current_datetime" >> $IPERF3_OUTPUT
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "" >> $IPERF3_OUTPUT
+
+    IPERF3_RESULTS=$(iperf3 -c $iperf3_server -p 5201)
+    echo "$IPERF3_RESULTS" >> $IPERF3_OUTPUT
+
+    # Run iperf3 test UDP
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "Running iperf3 UDP test at $current_datetime" >> $IPERF3_OUTPUT
+    echo "*************************************************************************" >> $IPERF3_OUTPUT
+    echo "" >> $IPERF3_OUTPUT
+
+    IPERF3_RESULTS=$(iperf3 -c $iperf3_server -u -b 50M -p 5201)
+    echo "$IPERF3_RESULTS" >> $IPERF3_OUTPUT
+
+    echo "Tests completed. Results saved to $IPERF3_OUTPUT."
+
+else
+    # If both value1 and value2 are false
+    echo "Both Ookla speedtest and iperf3 set not to run. To enable modify the device variable ookla_speedtest and  iperf3_speedtest to true"
+    # None of the instructions should run
+fi
